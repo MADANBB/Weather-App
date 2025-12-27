13d894ef649acae42ce086c37241872e002e979f
Madan <madanbb2004@gmail.com>
2025-12-27 11:21:57 +0530

    Update AQI calculation to Indian CPCB Standard

--- public/script.js
+++ public/script.js
@@ -10,6 +10,9 @@ async function getWeather() {
     const res = await fetch(`/api/weather?city=${location}`);
     const data = await res.json();
 
+    const pm25 = data.current.air_quality.pm2_5;
+    const indianAQI = getIndianAQI(pm25);
+
     document.getElementById("weatherCard").style.display = "block";
     document.getElementById("city").innerText =
       `${data.location.name}, ${data.location.country}`;
@@ -26,12 +29,23 @@ async function getWeather() {
     document.getElementById("wind").innerText =
       `${data.current.wind_kph} km/h`;
     document.getElementById("aqi").innerText =
-      data.current.air_quality["us-epa-index"];
+      `${indianAQI.aqi} (${indianAQI.label})`;
+;
 
   } catch {
     alert("City not found");
   }
 }
+
+function getIndianAQI(pm25) {
+  if (pm25 <= 30) return { aqi: 50, label: "Good 😊" };
+  if (pm25 <= 60) return { aqi: 100, label: "Satisfactory 🙂" };
+  if (pm25 <= 90) return { aqi: 200, label: "Moderate 😐" };
+  if (pm25 <= 120) return { aqi: 300, label: "Poor 😷" };
+  if (pm25 <= 250) return { aqi: 400, label: "Very Poor ☠️" };
+  return { aqi: 500, label: "Severe ☢️" };
+}
+
 
 document.querySelector("#search").addEventListener("click", getWeather);
 
